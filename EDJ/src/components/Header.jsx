1616423fc81f3505aa23fb648c4952e933e38e4a
Sara <sarasdj@stud.ntnu.no>
2025-02-06 13:11:16 +0100

    upadte: Header layout

---
+++ EDJ/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import '../styles/Header.css'
+
+function Header() {
+    return (
+        <div className="Header" id="Header">
+            <div className="Title">
+                <h1>E</h1>
+                <h2>VA</h2>
+                <h1>DJ</h1>
+            </div>
+            <div className="Box1"></div>
+            <div className="Box2"></div>
+            <div className="NavBar"></div>
+        </div>
+    )
+}
+
+export default Header
