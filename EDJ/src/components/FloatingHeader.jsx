17ca324302bb0d5d851d49d7ffb2dfafef807630
Sara <sarasdj@stud.ntnu.no>
2025-02-07 12:14:02 +0100

    update: link to floating header icons

--- EDJ/src/components/FloatingHeader.jsx
+++ EDJ/src/components/FloatingHeader.jsx
@@ -1,15 +1,23 @@
 import '../styles/FloatingHeader.css'
 
-import { FaSpotify, FaYoutube, FaFacebook } from "react-icons/fa";
-import { RiSoundcloudLine } from "react-icons/ri";
+import {RiFacebookCircleLine, RiSoundcloudLine, RiYoutubeLine} from "react-icons/ri";
+import {AiOutlineSpotify} from "react-icons/ai";
 
 function FloatingHeader() {
     return (
         <div className="FloatingHeader">
-            <FaSpotify className="HeaderIcon"><href></href></FaSpotify>
-            <FaYoutube className="HeaderIcon"><href></href></FaYoutube>
-            <RiSoundcloudLine className="HeaderIcon"><href></href></RiSoundcloudLine>
-            <FaFacebook className="HeaderIcon"><href></href></FaFacebook>
+            <a href="https://www.youtube.com" target="_blank">
+                <RiYoutubeLine className="HeaderIcon"/>
+            </a>
+            <a href="https://open.spotify.com" target="_blank">
+                <AiOutlineSpotify className="HeaderIcon"/>
+            </a>
+            <a href="https://www.facebook.com/" target="_blank">
+                <RiFacebookCircleLine className="HeaderIcon"/>
+            </a>
+            <a href="https://soundcloud.com" target="_blank">
+                <RiSoundcloudLine className="HeaderIcon"/>
+            </a>
             <div style={{width: '4rem'}}></div>
         </div>
     );
