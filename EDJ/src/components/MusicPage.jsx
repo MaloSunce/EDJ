e7d03ce09e8df102a13d91ae5a3ffc8fe0adc888
Sara <sarasdj@stud.ntnu.no>
2026-03-12 17:40:41 +0100

    fix(MusicPage): make partially responsive

--- EDJ/src/components/MusicPage.jsx
+++ EDJ/src/components/MusicPage.jsx
@@ -9,21 +9,20 @@ function MusicPage() {
 
     return (
         <div className="Music" id="Music">
-            <h1>MUSIC</h1>
-
-
-            {/* Selected display song */}
-            <div className="Discography">
-                <h2 className="SubTitle">Artist's choice</h2>
-                <div className="SelectedSongs">
-                    {
-                        Array(4).fill().map((_, index) =>
-                            <SongComponent key={index} index={index} />
-                        )
-                    }
+            <div className="OverlayBox">
+                <h1>MUSIC</h1>
+                <div className="Discography">
+                    <h2 className="SubTitle">Artist's choice</h2>
+                    <div className="SelectedSongs">
+                        {
+                            Array(4).fill().map((_, index) =>
+                                <SongComponent key={index} index={index} />
+                            )
+                        }
+                    </div>
                 </div>
             </div>
-            <div className="OverlayBox"></div>
+
             <div className="CompactDiskContainer">
                 <img src={cd} alt="Compact disk background image" />
                 <img src={cd} alt="Compact disk background image" />
