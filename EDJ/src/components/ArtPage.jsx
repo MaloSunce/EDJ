2133a0a7e1892c30fef07e8f21d7e4d8af8c2f53
Sara <sarasdj@stud.ntnu.no>
2026-03-15 17:11:54 +0100

    feat(ArtPage): began double row layout

--- EDJ/src/components/ArtPage.jsx
+++ EDJ/src/components/ArtPage.jsx
@@ -1,9 +1,50 @@
 import '../styles/ArtPage.css';
 
+
 function Art() {
+    const basePath = "../../public/Icons/"
+
+
+    const images = [
+        [
+            basePath + "star.png",
+            basePath + "star.png",
+            basePath + "star.png",
+            basePath + "star.png"
+        ],
+        [
+            basePath + "star.png",
+            basePath + "star.png",
+            basePath + "star.png",
+            basePath + "star.png"
+        ],
+        [
+            basePath + "star.png",
+            basePath + "star.png",
+            basePath + "star.png",
+            basePath + "star.png"
+        ]
+    ];
+
     return (
         <div className="Art" id="Art">
             <h1>ART</h1>
+
+            <div className="GalleryContainer">
+                <div className="Row1">{
+                    Array(6).fill().map((_, index) =>
+                        <img className="ArtPiece" src={images[0][index]} alt="Art piece" key={index} />
+                    )
+
+                }
+                </div>
+                <div className="Row2">{
+                    Array(7).fill().map((_, index) =>
+                        <img className="ArtPiece" src={images[1][index]} alt="Art piece" key={index} />
+                    )
+                }
+                </div>
+            </div>
         </div>
     );
 }
