9fca78a27c40235ed239640114e4a701d1c242fb
Sara <sarasdj@stud.ntnu.no>
2026-03-19 10:41:00 +0100

    feat(SongComponent): add title, album, and album cover from JSON file

--- EDJ/src/components/SongComponent.jsx
+++ EDJ/src/components/SongComponent.jsx
@@ -1,7 +1,7 @@
 import '../styles/SongComponent.css'
 import { SiAffinityphoto } from "react-icons/si";
 import { useState } from 'react';
-
+import tracks from '../../public/Music/tracks.json';
 
 
 function SongComponent({ index, activeIndex, setIndex }) {
@@ -25,16 +25,21 @@ function SongComponent({ index, activeIndex, setIndex }) {
         <div className="SongComponent">  {/*NB temporary flex display for placeholder icon*/}
             <div className="AlbumCover" style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
                 {/*NB placeholder icon*/}
-                <SiAffinityphoto style={{
-                    color: 'var(--text-primary)',
-                    opacity: '0.5',
-                    fontSize: '2rem',
+                <img src={`${tracks[index].albumCover}`} style={{
+                    width: '100%',
+                    height: '100%',
                     alt: 'Placeholder album cover'
-                }} />
+                }}/>
+                {/*<SiAffinityphoto style={{*/}
+                {/*    color: 'var(--text-primary)',*/}
+                {/*    opacity: '0.5',*/}
+                {/*    fontSize: '2rem',*/}
+                {/*    alt: 'Placeholder album cover'*/}
+                {/*}} />*/}
             </div>
             <div className="TextContainter">
-                <h3>Song Title</h3>
-                <p>From Album</p>
+                <h3>{tracks[index].title}</h3>
+                <p>From <i>{tracks[index].fromAlbum}</i></p>
             </div>
             <div style={{
                 margin: 'auto 0 auto auto',
